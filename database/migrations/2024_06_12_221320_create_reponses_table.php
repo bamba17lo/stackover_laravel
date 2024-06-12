77c5b7f7bae0d46cec8a0919e410316a78445a89
Bamba Lo <khadim221lo@gmail.com>
2024-06-12 22:37:08 +0000

    creation des tables

---
+++ database/migrations/2024_06_12_221320_create_reponses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reponses', function (Blueprint $table) {
+            $table->id();
+            $table->text('contenu');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
+            $table->foreignId('question_id')->constrained('questions')->onDelete('cascade');
+
+            $table->boolean('validation')->default(false);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reponses');
+    }
+};
