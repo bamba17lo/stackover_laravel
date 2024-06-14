a66157e037c3b10fb9b35830bc3d0528bde2608a
Bamba Lo <khadim221lo@gmail.com>
2024-06-14 03:09:16 +0000

    methode d ajout et suppression d'un theme

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Theme;
+use Exception;
 use Illuminate\Http\Request;
+
+use function PHPSTORM_META\map;
 
 class ThemeController extends Controller
 {
-    //
+    public function create(Request $request){
+        $data = $request->validate([
+                'libelle'=>'required|unique:themes,libelle'
+            ]);
+
+        try {
+            Theme::updateOrCreate(
+                ['libelle' => $data['libelle'] ],
+                ['libelle' => $data['libelle'] ]
+            );
+            return response()->json([
+                'code'=>200,
+                'message'=> 'success'
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'code'=>500,
+                'message'=> $e->getMessage()
+            ]);
+        }
+    }
+
+    public function delete($id){
+        $theme = Theme::find($id);
+        try {
+            if($theme){
+                $theme->delete();
+                return response()->json([
+                    'code' =>200,
+                    'message' => 'deleted success'
+                ]);
+            }
+        } catch (Exception $e){
+            return response()->json([
+                'code' =>500,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
 }
