cf7c809439a39b368bd6e2c301b628db50e90284
mamesayelom <mamesayelom04@gmail.com>
2024-06-15 20:32:12 +0100

    gestion des questions

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ThemeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Middleware\VerifyBearerToken;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +19,36 @@ use App\Http\Controllers\AuthController;
 */
 
 
-route::middleware(['auth:sanctum', SecuriseRoute::class])->group(function(){
+route::middleware(['auth:sanctum', VerifyBearerToken::class])->group(function(){
 
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    // Methode d'insertion du theme
+Route::post('storeTheme',[ThemeController::class,'create']);
+// Methode de suppression du theme
+Route::delete('deleteTheme/{id}',[ThemeController::class,'delete']);
+
+Route::post('/questions', [QuestionController::class, 'store']);
+
+Route::delete('/questions/{id}', [QuestionController::class, 'destroy']);
+Route::post('/questions/{id}', [QuestionController::class, 'update']);
+Route::get('/editquestion/{id}',[QuestionController::class,'edit']);
 
 
 
 });
+
+
 Route::post('/inscription', [AuthController::class,'register'])->name('inscription');
 Route::post('/login', [AuthController::class,'login'])->name("login");
+Route::get('/question', [QuestionController::class, 'index']);
+Route::get('/questions/{id}', [QuestionController::class, 'show']);
 
 
 
 
 
-// Methode d'insertion du theme
-Route::post('storeTheme',[ThemeController::class,'create']);
 
-// Methode de suppression du theme
-Route::get('deleteTheme/{id}',[ThemeController::class,'delete']);
+
+
+
